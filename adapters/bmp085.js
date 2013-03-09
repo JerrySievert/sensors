60ccc91f3c40c2940894ae5b7900f8932949146a
Jerry Sievert <code@legitimatesounding.com>
2013-03-08 20:43:40 -0800

    add wire to call

--- adapters/bmp085.js
+++ adapters/bmp085.js
@@ -2,7 +2,7 @@
 
 var calibrations = { };
 
-function readInteger(value, address, callback) {
+function readInteger(value, wire, address, callback) {
   wire.write(address, [ value ], function (err) {
     if (err) {
       callback(err);
@@ -22,37 +22,37 @@ function readInteger(value, address, callback) {
 
 function calibrate(wire, address, callback)
 {
-  readInteger(0xAA, address, function (err, res) {
+  readInteger(0xAA, wire, address, function (err, res) {
     calibrations['0xAA'] = res;
 
-    readInteger(0xAC, address, function (err, res) {
+    readInteger(0xAC, wire, address, function (err, res) {
       calibrations['0xAC'] = res;
 
-      readInteger(0xAE, address, function (err, res) {
+      readInteger(0xAE, wire, address, function (err, res) {
         calibrations['0xAE'] = res;
 
-        readInteger(0xB0, address, function (err, res) {
+        readInteger(0xB0, wire, address, function (err, res) {
           calibrations['0xB0'] = res;
 
-          readInteger(0xB2, address, function (err, res) {
+          readInteger(0xB2, wire, address, function (err, res) {
             calibrations['0xB2'] = res;
 
-            readInteger(0xB4, address, function (err, res) {
+            readInteger(0xB4, wire, address, function (err, res) {
               calibrations['0xB4'] = res;
 
-              readInteger(0xB6, address, function (err, res) {
+              readInteger(0xB6, wire, address, function (err, res) {
                 calibrations['0xB6'] = res;
 
-                readInteger(0xB8, address, function (err, res) {
+                readInteger(0xB8, wire, address, function (err, res) {
                   calibrations['0xB8'] = res;
 
-                  readInteger(0xBA, address, function (err, res) {
+                  readInteger(0xBA, wire, address, function (err, res) {
                     calibrations['0xBA'] = res;
 
-                    readInteger(0xBC, address, function (err, res) {
+                    readInteger(0xBC, wire, address, function (err, res) {
                       calibrations['0xBC'] = res;
 
-                      readInteger(0xBE, address, function (err, res) {
+                      readInteger(0xBE, wire, address, function (err, res) {
                         calibrations['0xBE'] = res;
 
                         callback(null, calibrations);
